9373e427e01205e3a4e7ca1864e0c0116db175c6
Ananya <thakurananyasingh2002@gmail.com>
2026-06-04 14:12:34 +0530

    Create smart_street_light.ino

---
+++ images/code/smart_street_light.ino
@@ -0,0 +1,18 @@
+int ldrPin = A0;
+int ledPin = 13;
+int ldrValue = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  ldrValue = analogRead(ldrPin);
+
+  if (ldrValue < 500) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
